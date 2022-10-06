d34d3248f855a53a7f9b804ef6bf31a31ab80e7b
gioanbatan <giovannibassani@hotmail.com>
2022-10-06 15:22:30 +0200

    Structure in pseudo-code created

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-alert("Eccallà");
+// Ciao ragazzi,
+// esercizio di oggi: calcolo del prezzo del biglietto del treno
+// cartella/repo js-biglietto-treno
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+/* INPUT */
+// Richiedere all'utente l'età
+// Richiedere all'utente il numero di chilometrio da percorrere
+
+/* ELABORAZIONE DATI */
+// Moltiplicare 0.21€ per i km da percorrere per ottenere il prezzo parziale
+
+// SE l'età dell'utente è minore di 18 ALLORA applicare 20% di sconto al prezzo parziale 
+// ALTRIMENTI SE l'età dell'utente è maggiore-uguale a 65 ALLORA applicare lo sconto del 40% di sconto al prezzo parziale 
+
+// Arrotondare il prezzo parziale a due cifre dopo la virgola per ottenere il prezzo finale
+
+/* OUTPUT */
+// Mostrare il prezzo finale del biglietto
