0a9b94233eb3724743aa099d2277d562feaa29a7
gioanbatan <giovannibassani@hotmail.com>
2022-10-06 16:00:42 +0200

    Data processing section created

--- js/script.js
+++ js/script.js
@@ -16,11 +16,25 @@ console.log(userAge, typeof(userAge), userDistance, typeof(userDistance));
 
 /* ELABORAZIONE DATI */
 // Moltiplicare 0.21€ per i km da percorrere per ottenere il prezzo parziale
+const pricePerKm = 0.21;
+let pricePartial = pricePerKm * userDistance;
+console.log(pricePartial);
 
 // SE l'età dell'utente è minore di 18 ALLORA applicare 20% di sconto al prezzo parziale 
-// ALTRIMENTI SE l'età dell'utente è maggiore-uguale a 65 ALLORA applicare lo sconto del 40% di sconto al prezzo parziale 
+const underDiscount = 20;
+const overDiscount = 40;
+
+if (userAge < 18) {
+    pricePartial -= ((pricePartial / 100) * underDiscount);
+} else if (userAge >= 65) {
+    // ALTRIMENTI SE l'età dell'utente è maggiore-uguale a 65 ALLORA applicare lo sconto del 40% di sconto al prezzo parziale 
+    pricePartial -= ((pricePartial / 100) * overDiscount);
+}
+console.log("pricePartial:", pricePartial);
 
 // Arrotondare il prezzo parziale a due cifre dopo la virgola per ottenere il prezzo finale
+const priceFinal = pricePartial.toFixed(2);
+console.log("priceFinal:", priceFinal);
 
 /* OUTPUT */
 // Mostrare il prezzo finale del biglietto
